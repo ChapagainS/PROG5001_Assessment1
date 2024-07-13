79f737f37d8b01992bd0db5824667ecabe56d113
ChapagainS <165454800+ChapagainS@users.noreply.github.com>
2024-07-13 20:34:34 +1000

    takeInput_Unit

--- PROG5001_Assessment1/MarkSheet.java
+++ PROG5001_Assessment1/MarkSheet.java
@@ -7,9 +7,11 @@
  */
 import java.util.Scanner;
 
-public class MarkSheet
-{
+public class MarkSheet {
+    
  public static void main(String[] args) {
+     System.out.println("Name of the Unit: ");
+     String unitName = (new Scanner(System.in)).nextLine();
      
  }
 }
